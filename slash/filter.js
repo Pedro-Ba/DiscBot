c783ff799189e47b844f1b23869ade58d2c5ec70
Pedro Barraza <pedropaschbarraza@gmail.com>
2023-03-14 20:31:35 -0300

    Add toggling audio filters

---
+++ slash/filter.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { useMasterPlayer } = require('discord-player');
+const player = useMasterPlayer();
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("filter")
+    .setDescription("toggles audio filters.")
+    .addStringOption((option) => {
+        option.setName('name')
+            .setDescription('the name of the filter to toggle.')
+            .setRequired(true)
+            return option;
+    }),
+    async execute(client, interaction) {
+        const queue = player.nodes.get(interaction.guildId);
+        let filterName = interaction.options.getString('name');
+        queue.filters.ffmpeg.toggle(filterName);
+        let disabledFilters = queue.filters.ffmpeg.getFiltersDisabled();
+        let enabledFilters = queue.filters.ffmpeg.getFiltersEnabled();
+        interaction.editReply(`The filter has been toggled. Here is the list of currently disabled filters: ${disabledFilters}, and the list of currently enabled filters: ${enabledFilters}`);
+    }
+}
