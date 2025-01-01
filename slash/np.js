ef3979cd8630543a7a51d2695a29919e7bf9c169
Estia <83793315+Pedro-Ba@users.noreply.github.com>
2025-01-01 13:43:19 -0300

    Fix: Timestamp to np

--- slash/np.js
+++ slash/np.js
@@ -8,10 +8,10 @@ module.exports = {
     .setDescription("Now Playing: Mostra a musica atualmente tocando."),
     async execute(client, interaction) {
         const queue = player.queues.create(interaction.guildId);
+        const timeline = useTimeline(interaction.guildId);
         const currentSong = queue.currentTrack;
         if(currentSong){
-            const timeline = useTimeline({node: interaction.guildId});
-            await interaction.editReply(`Música atual: \[${timeline.timestamp.current} | ${currentSong.duration}] ${currentSong.author} - ${currentSong.title}, ${currentSong.url}`);
+            await interaction.editReply(`Música atual: \[${timeline.timestamp.current.label} | ${currentSong.duration}] ${currentSong.author} - ${currentSong.title}, ${currentSong.url}`);
         }
         else{
             await interaction.editReply(`Seems like nothing is playing.`)
